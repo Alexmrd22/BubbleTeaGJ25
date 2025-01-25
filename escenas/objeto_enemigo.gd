7bc126fb0bae11d332ec8c1222431dcee6b12af9
Alejandro <148315626+Alexmrdgz@users.noreply.github.com>
2025-01-25 16:32:12 +0100

    Una situasion de la otia

--- escenas/objeto_enemigo.gd
+++
@@ -1,8 +0,0 @@
-extends RigidBody2D
-
-# No necesitas ningún código para que funcione la gravedad básica.
-# El motor ya la maneja automáticamente.
-
-func _ready() -> void:
-	# Configura la escala de la gravedad desde el código (opcional)
-	self.gravity_scale = gravity_scale
