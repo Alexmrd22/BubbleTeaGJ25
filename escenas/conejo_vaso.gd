84ffae9c16b81ce66b5eb4d613086abb34e77843
Alexmrdgz <alejandromir.2023.alumn@cescristorey.com>
2025-01-25 19:26:22 +0100

    Commit bomboclat yisus christ

--- escenas/conejo_vaso.gd
+++ escenas/conejo_vaso.gd
@@ -1,30 +1,27 @@
 extends CharacterBody2D
 
-
 const SPEED = 600.0
 const JUMP_VELOCITY = -400.0
-var anim_actual = "idle" # animacion actual: parado
+var anim_actual = null # animacion actual: parado
 
+func _ready() -> void:
+	$Sprite2D/AnimationPlayer.play("quieto")
+	anim_actual = "quieto"
 
 func _process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
-		cambiar_animacion("andando")
-		if velocity.x < 0:
-			$Sprite2D.flip_h = true
+		cambiar_animacion("andando")	
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = 0
+		cambiar_animacion("quieto")
 
 	move_and_slide()
 	
@@ -38,7 +35,6 @@ func cambiar_animacion(nombre_anim):
 		$Sprite2D/AnimationPlayer.play(nombre_anim)
 		anim_actual = nombre_anim
 	
-
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("burbuja"):
--- escenas/raspa_pescado.gd
+++ escenas/raspa_pescado.gd
@@ -1,6 +1,5 @@
 extends RigidBody2D
 
 func _ready() -> void:
-	# Configura la escala de la gravedad desde el código (opcional)
 	self.gravity_scale = gravity_scale
 	$Sprite2D/AnimationPlayer.play("giro")
