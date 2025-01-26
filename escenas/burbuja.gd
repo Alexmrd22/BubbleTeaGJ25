7e13f73ffb48f1a49033c9398a40d70660aa4113
Alejandro <148315626+Alexmrdgz@users.noreply.github.com>
2025-01-26 11:20:38 +0100

    Delete escenas directory

--- escenas/burbuja.gd
+++
@@ -1,9 +0,0 @@
-extends RigidBody2D
-
-func _ready() -> void:
-	# Configura la escala de la gravedad desde el código (opcional)
-	self.gravity_scale = gravity_scale
-	$Sprite2D/AnimationPlayer.play("giro")
-
-func cambiarAnimacionBurbuja():
-	$Sprite2D/AnimationPlayer.play("explotar")
