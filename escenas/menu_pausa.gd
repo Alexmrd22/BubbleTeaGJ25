cb918136a16a36b76269a005e143e6163591b14c
DavidPonsMoll <davidfrancescpon.2023.alumn@cescristorey.com>
2025-01-26 10:30:07 +0100

    Menu pausa razzclat

---
+++ escenas/menu_pausa.gd
@@ -0,0 +1,24 @@
+extends Control
+
+@onready var boton_reanudar = $VBoxContainer/ButtonReanudar
+@onready var boton_controles = $VBoxContainer/ButtonControles
+@onready var boton_menu = $VBoxContainer/ButtonMenuPrincipal
+	
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_reanudar_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_button_controles_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_button_menu_principal_pressed() -> void:
+	pass
